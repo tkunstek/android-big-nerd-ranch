261f6eabdcbb5176aec892a10591cb8b98e8b7da
Terrence Kunstek <terrence@kunstek.com>
2013-06-02 23:42:35 -0400

    Chapter 2 (page 31-44)

--- GeoQuiz/gen/com/bignerdranch/android/geoquiz/R.java
+++ GeoQuiz/gen/com/bignerdranch/android/geoquiz/R.java
@@ -24,9 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int false_button=0x7f080001;
-        public static final int true_button=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int false_button=0x7f080002;
+        public static final int next_button=0x7f080003;
+        public static final int question_text_view=0x7f080000;
+        public static final int true_button=0x7f080001;
     }
     public static final class layout {
         public static final int activity_quiz=0x7f030000;
@@ -37,11 +39,16 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int correct_toast=0x7f050004;
-        public static final int false_button=0x7f050003;
+        public static final int false_button=0x7f050002;
         public static final int incorrect_toast=0x7f050005;
         public static final int menu_settings=0x7f050006;
-        public static final int question_text=0x7f050001;
-        public static final int true_button=0x7f050002;
+        public static final int next_button=0x7f050003;
+        public static final int question_africa=0x7f050009;
+        public static final int question_americas=0x7f05000a;
+        public static final int question_asia=0x7f05000b;
+        public static final int question_mideast=0x7f050008;
+        public static final int question_oceans=0x7f050007;
+        public static final int true_button=0x7f050001;
     }
     public static final class style {
         /** 
--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -5,25 +5,59 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+		new TrueFalse(R.string.question_oceans, true),
+		new TrueFalse(R.string.question_mideast, false),
+		new TrueFalse(R.string.question_africa, false),
+		new TrueFalse(R.string.question_americas, true),
+		new TrueFalse(R.string.question_asia, true),
+	};
+	
+	private int mCurrentIndex = 0;
+	
+	private void updateQuestion() {
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+	
+	private void checkAnswer(boolean userPressedTrue) {
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		int messageResId = 0;
+		
+		if (userPressedTrue == answerIsTrue) {
+			messageResId = R.string.correct_toast;
+		} else {
+			messageResId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        updateQuestion();
+        
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
-				
+				checkAnswer(true);
 			}
 		});
         
@@ -32,10 +66,20 @@ public class QuizActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
-				
+				checkAnswer(false);
 			}
 		});
+        
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex +1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+        
     }
 
 
