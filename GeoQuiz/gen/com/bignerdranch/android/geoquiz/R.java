40c3da0094790c6bf5ba58cd5a6051f0f4906998
Terrence Kunstek <terrence@kunstek.com>
2013-06-02 22:26:24 -0400

    Chapter 1 (page 6-15)

--- GeoQuiz/gen/com/bignerdranch/android/geoquiz/R.java
+++ GeoQuiz/gen/com/bignerdranch/android/geoquiz/R.java
@@ -33,9 +33,11 @@ public final class R {
         public static final int quiz=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int false_button=0x7f050003;
+        public static final int menu_settings=0x7f050004;
+        public static final int question_text=0x7f050001;
+        public static final int true_button=0x7f050002;
     }
     public static final class style {
         /** 
