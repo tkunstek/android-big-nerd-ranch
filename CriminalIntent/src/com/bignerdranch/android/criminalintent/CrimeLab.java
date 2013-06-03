a49134020730f4b68f236bf82db8c031dcc2d9eb
Terrence Kunstek <terrence@kunstek.com>
2013-06-03 18:28:09 -0400

    Chapter 9 (page 167-170)

---
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,47 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.content.Context;
+
+public class CrimeLab {
+	private ArrayList<Crime> mCrimes;
+	
+	private static CrimeLab sCrimeLab;
+	private Context mAppContext;
+	
+	private CrimeLab(Context appContext) {
+		mAppContext = appContext;
+		mCrimes = new ArrayList<Crime>();
+		CreateCrimes(100);
+	}
+	
+	public static CrimeLab get(Context c) {
+		if (sCrimeLab == null) {
+			sCrimeLab = new CrimeLab(c.getApplicationContext());
+		}
+		return sCrimeLab;
+	}
+
+	public ArrayList<Crime> getCrimes() {
+		return mCrimes;
+	}
+	
+	public Crime getCrime(UUID id) {
+		for (Crime c : mCrimes) {
+			if (c.getId().equals(id))
+				return c;
+		}
+		return null;
+	}
+	
+	private void CreateCrimes(int count) {
+		for (int i=0; i < 100; i++) {
+			Crime c = new Crime();
+			c.setTitle("Crime #" + i);
+			c.setSolved(i %2 == 0); // Alternate
+			mCrimes.add(c);
+		}
+	}
+}
