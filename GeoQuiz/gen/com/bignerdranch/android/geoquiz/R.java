6efc2c29237a099e91a5be8fffff9bf046cb7258
Terrence Kunstek <terrence@kunstek.com>
2013-06-02 22:44:49 -0400

    Chapter 1 (page 15-20)

--- GeoQuiz/gen/com/bignerdranch/android/geoquiz/R.java
+++ GeoQuiz/gen/com/bignerdranch/android/geoquiz/R.java
@@ -24,7 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int false_button=0x7f080001;
+        public static final int true_button=0x7f080000;
     }
     public static final class layout {
         public static final int activity_quiz=0x7f030000;
--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,15 +1,22 @@
 package com.bignerdranch.android.geoquiz;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
 
 public class QuizActivity extends Activity {
 
+	private Button mTrueButton;
+	private Button mFalseButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mFalseButton = (Button)findViewById(R.id.false_button);
     }
 
 
