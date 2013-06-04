8b5e17c1e72115aab45188d42420adde69c86d71
Terrence Kunstek <terrence@kunstek.com>
2013-06-03 20:03:10 -0400

    Chapter 9 (page 170-177)

--- CriminalIntent/gen/com/bignerdranch/android/criminalintent/R.java
+++ CriminalIntent/gen/com/bignerdranch/android/criminalintent/R.java
@@ -30,7 +30,7 @@ public final class R {
         public static final int fragmentContainer=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_crime=0x7f030000;
+        public static final int activity_fragment=0x7f030000;
         public static final int fragment_crime=0x7f030001;
     }
     public static final class string {
@@ -39,6 +39,7 @@ public final class R {
         public static final int crime_solved_label=0x7f050005;
         public static final int crime_title_hint=0x7f050002;
         public static final int crime_title_label=0x7f050003;
+        public static final int crimes_title=0x7f050006;
         public static final int title_activity_crime=0x7f050001;
     }
     public static final class style {
---
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		return new CrimeListFragment();
+	}
+
+}
