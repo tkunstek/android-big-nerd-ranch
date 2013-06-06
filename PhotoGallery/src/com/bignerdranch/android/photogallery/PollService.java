773cc259430cc242ea71b0e90f852900ec4e7620
Terrence Kunstek <terrence@kunstek.com>
2013-06-06 17:20:10 -0400

    Chapter 29 (page 479-480)

--- PhotoGallery/src/com/bignerdranch/android/photogallery/PollService.java
+++ PhotoGallery/src/com/bignerdranch/android/photogallery/PollService.java
@@ -4,17 +4,21 @@ import java.util.ArrayList;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class PollService extends IntentService {
 	private static final String TAG = "PollService";
-	private static final int POLL_INTERVAL = 1000*15; // 15 seconds
+	private static final int POLL_INTERVAL = 1000*60*5; // 5 minutes
 	
 	public PollService() {
 		super(TAG);
@@ -43,8 +47,23 @@ public class PollService extends IntentService {
 		
 		if (!resultId.equals(lastResultId)) {
 			Log.i(TAG, "Got a new result: " + resultId);
-		} else {
-			Log.i(TAG, "Got a old result: " + resultId);
+			
+			Resources r = getResources();
+			PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this,PhotoGalleryActivity.class), 0);
+			
+			Notification notification = new NotificationCompat.Builder(this)
+				.setTicker(r.getString(R.string.new_pictures_title))
+				.setSmallIcon(android.R.drawable.ic_menu_report_image)
+				.setContentTitle(r.getString(R.string.new_pictures_title))
+				.setContentText(r.getString(R.string.new_pictures_text))
+				.setContentIntent(pi)
+				.setAutoCancel(true)
+				.build();
+			
+			NotificationManager notificationManager = (NotificationManager)
+					getSystemService(NOTIFICATION_SERVICE);
+			
+			notificationManager.notify(0, notification);
 		}
 		
 		prefs.edit()
